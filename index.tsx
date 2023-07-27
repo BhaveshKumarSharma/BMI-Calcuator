e1555fccdc3ca90319cca81384268ad60d386af9
bhavesh kumar sharma <bhaveshkumar.sharma@celebaltech.com>
2023-07-27 11:52:18 +0530

    BMI calculator

--- index.tsx
+++ index.tsx
@@ -1,15 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 
 const BMICalculator = () => {
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("male");
+  const [height, setHeight] = useState(150);
+  const [weight, setWeight] = useState(50);
+  const [age, setAge] = useState(20);
   const [bmiCategory, setBMICategory] = useState("");
 
   const calculateBMI = () => {
     const heightInMeters = height / 100;
     const bmi = weight / (heightInMeters * heightInMeters);
+    console.log(bmi);
     let category = "";
 
     if (bmi < 18.5) {
@@ -25,42 +27,64 @@ const BMICalculator = () => {
     setBMICategory(category);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
     calculateBMI();
   };
 
   return (
     <div>
       <h2>BMI Calculator</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Height (cm): </label>
-          <input
-            type="number"
-            value={height}
-            onChange={(e) => setHeight(e.target.value)}
-          />
+      <form onSubmit={handleSubmit} className="container mb-6">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-3 sm:col-span-1">
+            <label className="block bg-background text-foreground">
+              Height (cm):{" "}
+            </label>
+            <input
+              className="mt-1 block w-full rounded-md p-2 text-lg shadow-sm "
+              type="number"
+              value={height}
+              required
+              onChange={(event) => setHeight(Number(event.target.value))}
+            />
+          </div>
+          <div className="col-span-3 sm:col-span-1">
+            <label className="block bg-background text-foreground">
+              Weight (in kg):
+            </label>
+            <input
+              className="mt-1 block w-full rounded-md p-2 text-lg shadow-sm "
+              type="number"
+              value={weight}
+              required
+              onChange={(event) => setWeight(Number(event.target.value))}
+            />
+          </div>
+          <div className="col-span-3 sm:col-span-1">
+            <label className="block bg-background text-foreground">Age:</label>
+            <input
+              className="mt-1 block w-full rounded-md p-2 text-lg shadow-sm "
+              type="number"
+              value={age}
+              required
+              onChange={(event) => setAge(Number(event.target.value))}
+            />
+          </div>
+
+          <div className="col-span-3">
+            <Button type="submit">Calculate BMI</Button>
+          </div>
         </div>
-        <div>
-          <label>Weight (kg): </label>
-          <input
-            type="number"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Age (years): </label>
-          <input
-            type="number"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-          />
-        </div>
-        <button type="submit">Calculate BMI</button>
       </form>
-      {bmiCategory && <p>Your BMI category is: {bmiCategory}</p>}
+
+      <div className="mt-4">
+        {bmiCategory && (
+          <p className="text-lg font-mono font-semibold">
+            Your BMI category is: {bmiCategory}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
